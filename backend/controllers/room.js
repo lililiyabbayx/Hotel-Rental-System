8507300c6a75dd021c31c82066df872d27c6e281
Rifah <rifahjahan2000@gmail.com>
2024-12-15 22:49:32 +0600

    updated

---
+++ backend/controllers/room.js
@@ -0,0 +1,66 @@
+import Room from "../models/Room.js";
+import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+
+export const createRoom = async (req,res,next) =>{
+    const hotelId=req.params.hotelid;
+    const newRoom=new Room(req.body)
+
+    try{
+        const savedRoom=await newRoom.save()
+        try{
+            await Hotel.findByIdAndUpdate(hotelId,{$push:{rooms:savedRoom._id}});
+
+        }catch(err){
+            next(err);
+        }
+        res.status(200).json(savedRoom);
+        
+    }catch(err){
+        next(err);
+    }
+
+};
+export const updateRoom = async (req, res, next) => {
+  try {
+    const updatedRoom = await Room.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    ); //mongodb set method, it gonna update first then return the new update
+    res.status(200).json(updatedRoom); //200 ok
+  } catch (err) {
+    next(err); //server side 500
+  }
+};
+
+export const getroom = async (req, res, next) => {
+  try {
+    const room = await Room.findById(req.params.id); //find hotel by id
+    res.status(200).json(room); //200 ok if successfull return hotel
+  } catch (err) {
+    next(err); //server side 500
+  }
+};
+
+export const getHotels = async (req, res, next) => {
+  try {
+    const rooms = await Room.find(); //(req.params.id); //find hotel by id
+    res.status(200).json(rooms); //200 ok if successfull return hotel
+  } catch (err) {
+    next(err); //server side 500
+  }
+};
+
+export const deleteRoom = async (req, res, next) => {
+  try {
+    await Room.findByIdAndDelete(req.params.id);
+    res.status(200).json("deleted Room successfully"); //200 ok just send a message
+  } catch (err) {
+    next(err); //server side 500
+  }
+};
+
+
--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -1,5 +1,29 @@
 import express from "express";
 
-const router = express.Router();
 
+import { createRoom,
+        deleteRoom,
+        getRooms,
+        getRoom,
+        updateRoom} from "../controllers/room.js";
+
+const router = express.Router();
+//create :
+// localhost:5500/api/hotels
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+// update :
+// localhost:5500/api/hotels/675b4ede04a315264cb82e35
+router.put("/:id", verifyAdmin, updateRoom);
+// get :
+// localhost:5500/api/hotels/675b4ede04a315264cb82e35
+router.get("/:id", getRoom);
+// get:
+//  all localhost:5500/api/hotels
+//
+router.get("/:id", getRooms);
+// delete:
+// localhost:5500/api/hotels/675b5691fc25021c925f68ce
+router.delete("/:id", verifyAdmin, deleteRoom);
 export default router;
+
