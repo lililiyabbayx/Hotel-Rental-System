8575cac823cf9d83a31f8ebba635f91da3eb5f82
Rifah <rifahjahan2000@gmail.com>
2024-12-20 12:43:38 +0600

    updated

--- backend/controllers/hotel.js
+++ backend/controllers/hotel.js
@@ -1,55 +1,99 @@
 import Hotel from "../models/Hotel.js";
+import Room from "../models/Room.js";
 
 export const createHotel = async (req, res, next) => {
-  //using async it will add data it will take time
-  //take hotel info from user
-  const newHotel = new Hotel(req.body); //req.body req from user
+  const newHotel = new Hotel(req.body);
+
   try {
-    const savedHotel = await newHotel.save(); //save hotel
-    res.status(200).json(savedHotel); //200 ok
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
   } catch (err) {
-    next(err); //server side 500
+    next(err);
   }
 };
-
 export const updateHotel = async (req, res, next) => {
   try {
     const updatedHotel = await Hotel.findByIdAndUpdate(
       req.params.id,
-      {
-        $set: req.body,
-      },
+      { $set: req.body },
       { new: true }
-    ); //mongodb set method, it gonna update first then return the new update
-    res.status(200).json(updatedHotel); //200 ok
+    );
+    res.status(200).json(updatedHotel);
   } catch (err) {
-    next(err); //server side 500
+    next(err);
   }
 };
-
-export const getHotel = async (req, res, next) => {
-  try {
-    const hotel = await Hotel.findById(req.params.id); //find hotel by id
-    res.status(200).json(hotel); //200 ok if successfull return hotel
-  } catch (err) {
-    next(err); //server side 500
-  }
-};
-
-export const getHotels = async (req, res, next) => {
-  try {
-    const hotels = await Hotel.find(); //(req.params.id); //find hotel by id
-    res.status(200).json(hotels); //200 ok if successfull return hotel
-  } catch (err) {
-    next(err); //server side 500
-  }
-};
-
 export const deleteHotel = async (req, res, next) => {
   try {
     await Hotel.findByIdAndDelete(req.params.id);
-    res.status(200).json("deleted Hotel successfully"); //200 ok just send a message
+    res.status(200).json("Hotel has been deleted.");
   } catch (err) {
-    next(err); //server side 500
+    next(err);
+  }
+};
+export const getHotel = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    next(err);
+  }
+};
+export const getHotels = async (req, res, next) => {
+  const { min, max, ...others } = req.query;
+  try {
+    const hotels = await Hotel.find({
+      ...others,
+      cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+    }).limit(req.query.limit);
+    res.status(200).json(hotels);
+  } catch (err) {
+    next(err);
+  }
+};
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(",");
+  try {
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
+    res.status(200).json(list);
+  } catch (err) {
+    next(err);
+  }
+};
+export const countByType = async (req, res, next) => {
+  try {
+    const hotelCount = await Hotel.countDocuments({ type: "hotel" });
+    const apartmentCount = await Hotel.countDocuments({ type: "apartment" });
+    const resortCount = await Hotel.countDocuments({ type: "resort" });
+    const villaCount = await Hotel.countDocuments({ type: "villa" });
+    const cabinCount = await Hotel.countDocuments({ type: "cabin" });
+
+    res.status(200).json([
+      { type: "hotel", count: hotelCount },
+      { type: "apartments", count: apartmentCount },
+      { type: "resorts", count: resortCount },
+      { type: "villas", count: villaCount },
+      { type: "cabins", count: cabinCount },
+    ]);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getHotelRooms = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    const list = await Promise.all(
+      hotel.rooms.map((room) => {
+        return Room.findById(room);
+      })
+    );
+    res.status(200).json(list)
+  } catch (err) {
+    next(err);
   }
 };
